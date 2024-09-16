0128fc0cebd9837c30d1211b7e01591da21e4662
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 19:26:18 -0700

    comments added

--- names_arrays.cc
+++ names_arrays.cc
@@ -11,13 +11,13 @@ int main() {
     string names[3]; //empty array created for inputs
 
     cout << "Enter the first name: ";
-    getline(cin, names[0]);  
+    getline(cin, names[0]);   // this input stores the first name in the array
 
     cout << "Enter the second name: ";
-    getline(cin, names[1]);  
+    getline(cin, names[1]);   // this input stores the second name in the array
 
     cout << "Enter the third name: ";
-    getline(cin, names[2]);  
+    getline(cin, names[2]);   // this input stores the third name in the array
 
 
     cout << "The first name is: " << names[0] << endl;
